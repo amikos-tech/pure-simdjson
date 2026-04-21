b7f0dc70d0f18fa8165bde41ec0849fded251a38
Trayan Azarov <trayan.azarov@amikos.tech>
2026-04-21 10:20:59 +0300

    feat(06-04): add reusable release smoke scripts
    - add staged release-tree assembly and bootstrap smoke entrypoints
    - add ABI surface and Go consumer smoke harnesses

---
+++ tests/smoke/go_bootstrap_smoke.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	purejson "github.com/amikos-tech/pure-simdjson"
+)
+
+func failf(format string, args ...any) {
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
+	os.Exit(1)
+}
+
+func main() {
+	parser, err := purejson.NewParser()
+	if err != nil {
+		failf("NewParser(): %v", err)
+	}
+	defer func() {
+		if err := parser.Close(); err != nil {
+			failf("parser.Close(): %v", err)
+		}
+	}()
+
+	doc, err := parser.Parse([]byte("42"))
+	if err != nil {
+		failf("Parse(42): %v", err)
+	}
+	defer func() {
+		if err := doc.Close(); err != nil {
+			failf("doc.Close(): %v", err)
+		}
+	}()
+
+	value, err := doc.Root().GetInt64()
+	if err != nil {
+		failf("Root().GetInt64(): %v", err)
+	}
+	if value != 42 {
+		failf("Root().GetInt64() = %d, want 42", value)
+	}
+}
